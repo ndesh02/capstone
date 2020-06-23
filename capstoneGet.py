86a4acde7e4f86c41af6329a7daf5879f6f183b1
ndesh02 <54901274+ndesh02@users.noreply.github.com>
2020-06-23 18:50:18 -0400

    Added some more columns and fixed some bugs

--- capstoneGet.py
+++ capstoneGet.py
@@ -41,9 +41,10 @@ except:
 conn = sqlite3.connect('content.sqlite')
 cur = conn.cursor()
 
+cur.execute('DROP TABLE Meteors')
 #Creates the sqlite database
 cur.execute('''CREATE TABLE IF NOT EXISTS Meteors
-    (id INTEGER PRIMARY KEY, name TEXT UNIQUE, nameType BOOLEAN, reclass TEXT, mass INTEGER,
+    (id INTEGER PRIMARY KEY, name TEXT UNIQUE, nameType TEXT, recclass TEXT, mass INTEGER,
      fall TEXT, reclat INTEGER, reclong INTEGER, year INTEGER, geolocation TEXT UNIQUE)''')
 
 meteor = dict()
@@ -51,7 +52,7 @@ count = 0
 name = None
 id = None
 nameType = None
-reclass = None
+recclass = None
 mass = None
 fall = None
 reclat = None
@@ -62,21 +63,58 @@ for item in js:
     meteor = js[count]
     try:
         name = meteor["name"]
+    except:
+        print("===Name was missing===")
+
+    try:
         id = meteor["id"]
-        nameType = meteor["nameType"]
-        reclass = meteor["reclass"]
-        mass = meteor["mass"]
+    except:
+        print('===ID was missing===')
+
+    try:
+        nameType = meteor["nametype"]
+    except:
+        print('===NameType was missing===')
+
+    try:
+        recclass = meteor["recclass"]
+    except:
+        print('===Reclass was missing===')
+
+    try:
         fall = meteor["fall"]
+    except:
+        print('===Fall was missing===')
+
+    try:
         reclat = meteor["reclat"]
+    except:
+        print('===Reclat was missing===')
+
+    try:
         reclong = meteor["reclong"]
+    except:
+        print("===Reclong was missing===")
+
+    try:
         year = meteor["year"]
         year = year[:4]
+    except:
+        print('===Year was missing===')
+
+    try:
         geolocation = meteor["geolocation"]["latitude"]+" "+meteor["geolocation"]["longitude"]
     except:
-        print('===One or more parameters was missing===')
-    print(id, name, mass, year, geolocation)
-    cur.execute('''INSERT OR IGNORE INTO Meteors (id, name, mass, year, geolocation)
-        VALUES ( ?, ?, ?, ?, ?)''', ( id, name, mass, year, geolocation))
+        print('===Geolocation was missing===')
+
+    try:
+        mass = meteor["mass"]
+    except:
+        print('===Mass was missing===')
+
+    print(id, name, nameType, recclass, mass, fall, reclat, reclong, year, geolocation)
+    cur.execute('''INSERT OR IGNORE INTO Meteors (id, name, nameType, recclass, mass, fall, reclat, reclong, year, geolocation)
+        VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (id, name, nameType, recclass, mass, fall, reclat, reclong, year, geolocation))
     if count%50 == 0: conn.commit()
     count = count + 1
 
